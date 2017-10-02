a2223f45a727150f2095760dcb22d5f7c26f6e41
CJohanB <carljohanbonde@gmail.com>
2017-10-02 11:17:36 +0200

    World Generator

---
+++ Project Eclipse/Assets/Scripts/WorldGenerate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerate : MonoBehaviour {
+
+    
+    public int width;
+    public int heigt = 2;
+
+    public GameObject block;
+
+	void Start ()
+    {
+        Generate();
+    }
+	
+	
+    //En funktion som genererar världen.
+    void Generate()
+    {
+        //Loopar igenom bredden.
+        for (int i = 0; i < width; i++)
+        {
+            //Bestämmer en lokal variabel, "heigt2", som är världens höjd. Heigt2 blir ett värde mellan "heigt" och heigt +4.
+            int heigt2 = Random.Range(heigt, heigt + 4);
+            for (int j = 0; j < heigt2; j++)
+            {
+                Instantiate(block, new Vector3(i, j), Quaternion.identity);
+            }
+        }
+
+    }
+}
