e0716b43ae52014a69eeda3bf24b16d51c3e5d31
CJohanB <carljohanbonde@gmail.com>
2017-10-02 19:42:09 +0200

    Test

--- Project Eclipse/Assets/Scripts/WorldGenerate.cs
+++ Project Eclipse/Assets/Scripts/WorldGenerate.cs
@@ -16,7 +16,7 @@ public class WorldGenerate : MonoBehaviour {
 
     public GameObject block;
     public GameObject startBlock;
-
+    
 	void Start ()
     {
         Generate();
@@ -33,9 +33,7 @@ public class WorldGenerate : MonoBehaviour {
         for (int i = 0; i < width; i++)
         {
             //Bestämmer en lokal variabel, "heigt2", som är världens höjd. Heigt2 blir ett värde mellan "heigt" och heigt +4.
-
             int h = Mathf.RoundToInt(Mathf.PerlinNoise(seed, i / smoothness) * heightMultiplier) + heigtAddition;
-            Debug.Log(h);
             for (int j = 0; j < h; j++)
             {
                 GameObject newBlock = Instantiate(block, new Vector3(i, j), Quaternion.identity);
