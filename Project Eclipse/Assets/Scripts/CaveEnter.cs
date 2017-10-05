cf44f77d53202eb007538e7b833a8352c326ee0d
CJohanB <carljohanbonde@gmail.com>
2017-10-05 10:00:38 +0200

    123

---
+++ Project Eclipse/Assets/Scripts/CaveEnter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CaveEnter : MonoBehaviour {
+
+    public GameObject uiButton;
+    public string caveLevel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player enterd collider.");
+            // uiButton.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene(caveLevel);
+            } 
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+           // uiButton.SetActive(false); 
+        }
+    }
+}
