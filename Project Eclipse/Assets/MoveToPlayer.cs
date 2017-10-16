18cbd19b506cdf7ab2b9227d9739409800bc11bd
CJohanB <carljohanbonde@gmail.com>
2017-10-16 11:21:30 +0200

    123

--- Project Eclipse/Assets/MoveToPlayer.cs
+++ Project Eclipse/Assets/MoveToPlayer.cs
@@ -7,30 +7,55 @@ public class MoveToPlayer : MonoBehaviour {
     public GameObject player;
     private Rigidbody2D rb;
     public LayerMask layerMask;
+    public Vector3 speed;
+    public bool facingRight = true;
 
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody2D>();
+    // Use this for initialization
+    void Start () {
+       rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Obstacle")
+        {
+            Debug.Log("JAG HATAR LIVET!");
+           // if (Vector3.Distance(transform.position, other.transform.position) > 2)
+            //{
+                rb.AddForce(new Vector3(0, 1000, 0));
+           // }
+
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
         float dis = Vector3.Distance(transform.position, player.transform.position);
-        Debug.Log(dis);
-  
-        Vector3 dir = (player.transform.position - transform.position);
-       
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, layerMask);
+        //Debug.Log(dis);
 
-        Debug.Log(hit.transform.name);
-        if (hit.collider.tag == "Obstacle" && Vector3.Distance (transform.position, hit.transform.position) > 2)
+        Vector3 dir = (player.transform.position - transform.position);
+
+        rb.AddForce(dir * 2);
+
+        if (dir.x > 0 && !facingRight)
         {
-            rb.AddForce(Vector3.up);
+            Flip();
         }
-        if (dis > 2)
+
+        if (dir.x < 0 && facingRight)
         {
-            rb.AddForce(dir* 2);
+            Flip();
         }
-	}
+
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 newScale = transform.localScale;
+        newScale.x *= -1;
+        transform.localScale = newScale;
+    }
 }
