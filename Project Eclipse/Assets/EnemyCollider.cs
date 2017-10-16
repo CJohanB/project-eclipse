4e398afd12cba001bce46a56eab547b083eac979
Xscalze <dr.xscalze@gmail.com>
2017-10-16 11:10:38 +0200

    Done

---
+++ Project Eclipse/Assets/EnemyCollider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour {
+
+    [SerializeField] private float ParantHealth;
+
+    [SerializeField] GameObject DangerZone;
+
+    [SerializeField] bool DamageAble;
+
+	// Use this for initialization
+	void Start () {
+
+        ParantHealth = GetComponentInParent<EnemyStats>().Health;
+        DangerZone = GameObject.FindGameObjectWithTag("DangerZone");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (DangerZone)
+        {
+            DamageAble = true;
+        }
+
+    }
+
+}
