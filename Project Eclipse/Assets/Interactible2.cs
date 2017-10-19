e02b74e1e4c3c0b15ce3519fc234f08eea6ee54c
CJohanB <carljohanbonde@gmail.com>
2017-10-19 11:13:30 +0200

    EFFECTS!

---
+++ Project Eclipse/Assets/Interactible2.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Interactible2 : MonoBehaviour {
+
+    public Effect effect = new Effect();
+
+    bool incontact = false;
+    bool itemUsed = false;
+
+    GameObject GameMaster;
+    GameObject Player;
+
+    public int speedBoost;
+    public int jumpBost;
+    public float timeLimit;
+
+    public void Start()
+    {
+
+        effect.speedBoost += speedBoost;
+        effect.jumpBost += jumpBost;
+        effect.timeLimit += timeLimit;
+        GameMaster = GameObject.FindGameObjectWithTag("GameMaster");
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+
+    }
+
+    private void Update()
+    {
+       
+
+        if (incontact == true && itemUsed == false)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                itemUsed = true;
+                GameMaster.GetComponent<cheackEffects>().Cheack(effect);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            incontact = true;
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            incontact = false;
+        }
+    }
+
+}
---
+++ Project Eclipse/Assets/cheackEffects.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cheackEffects : MonoBehaviour {
+
+    public GameObject player;
+
+    public List<Effect> effects = new List<Effect>();
+
+    public void Cheack(Effect newEffect)
+    {
+
+        effects.Add(newEffect);
+   
+    }
+
+    public void DoTheThings()
+    {
+        
+            foreach (Effect e in effects)
+            {
+                if (e.hasBeenActivated != true)
+                {
+                    player.GetComponent<PlayerMove>().maxSpeed += e.speedBoost;
+                    player.GetComponent<PlayerMove>().jumpForce += e.jumpBost;
+                    e.hasBeenActivated = true;
+                }
+            }
+        
+       
+    }
+
+    void Update ()
+    {
+        //Debug.Log(effects.Count);
+        if (effects.Count != 0)
+            DoTheThings();
+        
+        foreach (Effect e in effects)
+        {
+            e.timeLimit -= Time.deltaTime;
+
+            if (e.timeLimit <= 0)
+            {
+                Debug.Log("removing effect");
+                RemoveEffect(e);
+                effects.Remove(e);
+
+            }
+        }
+    }
+
+
+    private void RemoveEffect(Effect e)
+    {
+        player.GetComponent<PlayerMove>().maxSpeed -= e.speedBoost;
+        player.GetComponent<PlayerMove>().jumpForce -= e.jumpBost;
+    }
+}
